ed869514732dadf76809f269f043b527c714fb5d
Atlas <atlas.guo@wisc.edu>
2024-02-26 12:21:12 -0600

    major update of portfolio maps and structure adjustment

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
 	// Fetch and load external HTML content
-	fetch("portfolio.html")
+	fetch("html/portfolio.html")
 		.then(response => response.text())
 		.then(data => {
 			document.getElementById("portfolio-detail").innerHTML = data;
