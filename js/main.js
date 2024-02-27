6c137537e03d272edffbbbfc12dc070a65056c93
Atlas <atlas.guo@wisc.edu>
2024-02-26 18:26:51 -0600

    removing embedded mapbox

--- js/main.js
+++ js/main.js
@@ -4,5 +4,10 @@ document.addEventListener("DOMContentLoaded", function () {
 		.then(response => response.text())
 		.then(data => {
 			document.getElementById("portfolio-detail").innerHTML = data;
+			//console.log("Portfolio html file is loaded.")
 		});
 });
+
+window.onload = function () {
+	console.log("Everything is loaded.")
+};
