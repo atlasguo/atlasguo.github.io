0e0c17a9f88c32f0695cbde9217137fe076c62f8
Atlas <atlas.guo@wisc.edu>
2024-05-25 00:16:51 -0500

    testing counter

--- js/main.js
+++ js/main.js
@@ -9,9 +9,19 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 window.onload = function () {
-	var widgetElement = document.getElementById('mapmyvisitors-widget');
-	if (widgetElement) {
-		widgetElement.style.display = 'none';
+	var widgetElement = document.getElementsByClassName('jvectormap-marker');
+	if (widgetElement.length > 0) {
+		widgetElement[0].setAttribute('r', '0');
 	}
+
+	var iframe = document.querySelector('iframe');
+	console.log(iframe);
+	if (iframe) {
+		iframe.style.display = 'none';
+		//iframe.id = 'myIframe'; // Assign an ID
+		// Or assign a class
+		// iframe.classList.add('myIframeClass');
+	}
+
 	console.log("Everything is loaded.")
 };
