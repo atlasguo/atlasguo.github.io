f1b6cbc9f93a5859fb46ff13f49c6556cc209e8b
Atlas <atlas.guo@wisc.edu>
2024-05-25 03:32:28 -0500

    allow accessing individual map with url

--- js/main.js
+++ js/main.js
@@ -12,16 +12,34 @@ window.onload = function () {
 
 	$(document).ready(function () {
 
-		$('.jvectormap-marker').css('display', 'none');
-		$('#mapmyvisitors-widget').removeAttr('href').css({
-			'cursor': 'default'
-		});
-		//$('#mapmyvisitors-widget').hide();
+		// $('.jvectormap-marker').css('display', 'none');
+		// $('#mapmyvisitors-widget').removeAttr('href').css({
+		// 	'cursor': 'default'
+		// });
+		// //$('#mapmyvisitors-widget').hide();
 
-		$('iframe').removeAttr('href').css({
-			'cursor': 'default'
+		// $('iframe').removeAttr('href').css({
+		// 	'cursor': 'default'
+		// });
+		// //$('iframe').hide();
+
+		var elementsWithId = document.querySelectorAll('[id]');
+		var ids = [];
+
+		elementsWithId.forEach(function (element) {
+			if (element.id.substring(0, 10) == "portfolio_") {
+				ids.push('#' + element.id);
+			}
 		});
-		//$('iframe').hide();
+
+		var ids = ids.filter(function (id) {
+			return id !== "#portfolio_TEMPLATE";
+		});
+
+		var hash = window.location.hash;
+		if (ids.includes(hash)) {
+			$(hash).modal('show');
+		}
 	});
 
 	console.log("Everything is loaded.")
