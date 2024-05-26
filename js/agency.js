f70385ac69c63a1b6a3b02f2e175963ae8973af5
Atlas <atlas.guo@wisc.edu>
2024-05-25 19:03:34 -0500

    Add filtering feature for portfolio

--- js/agency.js
+++ js/agency.js
@@ -47,4 +47,33 @@
 		$('.navbar').removeClass('d-none');
 	})
 
+	var siteIstotope = function () {
+		var $container = $('#portfolio-grid').isotope({
+			itemSelector: '.portfolio-item',
+			isFitWidth: true,
+			layoutMode: 'fitRows'
+		});
+
+		$(window).resize(function () {
+			$container.isotope({
+				columnWidth: '.col-sm-3'
+			});
+		});
+
+		$container.isotope({ filter: '*' });
+
+		$('#filters').on('click', 'a', function (e) {
+			e.preventDefault();
+			var filterValue = $(this).attr('data-filter');
+			$container.isotope({ filter: filterValue });
+			$('#filters a').removeClass('active');
+			$(this).addClass('active');
+		});
+	}
+
+	$(window).on('load', function () {
+		siteIstotope();
+	});
+
 })(jQuery); // End of use strict
+
