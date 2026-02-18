46656db5bd7d4839a582d702a1d2bb0d308e42be
Atlas <chenxiao.atlas.guo@gmail.com>
2026-02-17 22:00:55 -0500

    Create separate folder for LEGO maps

--- scripts/install-githooks.ps1
+++
@@ -1,6 +0,0 @@
-﻿$ErrorActionPreference = "Stop"
-Set-Location -LiteralPath (Split-Path -Parent $MyInvocation.MyCommand.Path)
-Set-Location -LiteralPath ..
-
-git config core.hooksPath .githooks
-Write-Output "Configured git hooks path: .githooks"
